ea0127c16e32d943faf8296f9bd23a1eea0f3f52
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2025-12-10 21:14:17 -0300

    Feat: Ajsutes no gitignore, conexão com banco de dados

---
+++ src/test/java/com/todo_list/demo/DatabaseConnectionTest.java
@@ -0,0 +1,26 @@
+package com.todo_list.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class DatabaseConnectionTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    void testDattabaseConnection() throws Exception {
+        Connection connection  = dataSource.getConnection();
+        assertNotNull(connection);
+        System.out.println("Conectado com o baanco de dados");
+        connection.close();
+    }
+
+}
