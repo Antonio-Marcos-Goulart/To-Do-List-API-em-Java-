997daf3d9ead942870bac92e86035d2bcc85aa56
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2025-12-18 17:08:57 -0300

    feat: create task controller with validations and error messages

---
+++ src/main/java/com/todo_list/demo/controller/TaskController.java
@@ -0,0 +1,70 @@
+package com.todo_list.demo.controller;
+
+import com.todo_list.demo.DTOS.TasksDTO;
+import com.todo_list.demo.model.Task;
+import com.todo_list.demo.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @Autowired
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @Operation(summary = "Create task", description = "Create task")
+    @PostMapping
+    public ResponseEntity<Task> createTask(@Valid @RequestBody TasksDTO dto) {
+        Task saved = taskService.createTask(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @Operation(summary = "Update task by id", description = "Update an existing task by its id")
+    @PutMapping("/{id}")
+    public ResponseEntity<Task> updateTask(
+            @PathVariable Long id,
+            @Valid @RequestBody TasksDTO dto) {
+        Task updatedTask = taskService.updateTask(id, dto);
+        return ResponseEntity.ok(updatedTask);
+    }
+
+    @Operation(summary = "List all tasks", description = "List all tasks")
+    @GetMapping("/all_tasks")
+    public List<TasksDTO> getAllTasks(){
+        return taskService.getAllTasks().stream().map(TasksDTO::new).collect(Collectors.toList());
+    }
+
+    @Operation(summary = "Delete tasks by Id", description = "Delete tasks by Id")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Search task by Id", description = "Search tasks by Id")
+    @GetMapping("/{id}")
+    public ResponseEntity<TasksDTO> getTaskById(@PathVariable Long id) {
+        Task  task = taskService.getTaskById(id);
+        return ResponseEntity.ok(new TasksDTO(task));
+    }
+
+    @Operation(summary = "Search task by title and Id", description = "Search tasks by title and Id")
+    @GetMapping("/search")
+    public ResponseEntity<List<Task>> searchTasks(
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) String title) {
+        return ResponseEntity.ok(taskService.searchTask(id, title));
+    }
+}
