27b075156135f660b7dc440d387a58ef0c5b6a6f
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2025-12-18 17:12:45 -0300

    feat: add query methods in the task repository

--- src/main/java/com/todo_list/demo/repository/TaskRepository.java
+++ src/main/java/com/todo_list/demo/repository/TaskRepository.java
@@ -5,9 +5,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findByTaskTitleContainingIgnoreCase(String title);
+
+    List<Task> findByTaskGroup_Id(Long taskGroupId);
+
+    List<Task> findByTaskGroup_IdAndTaskTitleContainingIgnoreCase(
+            Long taskGroupId,
+            String title
+    );
+
+    Optional<Task> findByTaskIdAndTaskGroup_Id(
+            Long taskId,
+            Long taskGroupId
+    );
 }
